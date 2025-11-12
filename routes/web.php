b5a6ec9051c40827cb884f5a9d6c40bec9708e56
info@ingsot.com <igor@dev2025.ingsot.com>
2025-11-12 09:47:46 +0200

    ***

--- routes/web.php
+++ routes/web.php
@@ -15,37 +15,35 @@ Route::get('/test-styles', function () {
 
 Route::get('/asana-test-projects', [\App\Http\Controllers\AsanaTestController::class, 'projects']);
 
-Route::post('/viber/webhook', function (Request $request) {
-    file_put_contents(storage_path('logs/test_viber.txt'), "Webhook вызван\n", FILE_APPEND);
-    return response()->json(['status' => 0]);
-});
-
-//Route::post('/viber/webhook', function (Request $request) {
-//    $data = $request->all();
-//
-//    // Логируем всё в storage/logs/viber_log.txt
-//    Storage::disk('logs')->append('viber_log.txt', json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-//
-//    // Если это обычное сообщение от пользователя
-//    if (isset($data['event']) && $data['event'] === 'message') {
-//        $userId = $data['sender']['id'] ?? null;
-//        $userName = $data['sender']['name'] ?? 'Без имени';
-//        $text = $data['message']['text'] ?? '';
-//
-//        if ($userId) {
-//            Http::withHeaders([
-//                'X-Viber-Auth-Token' => '479d6bb020e7d3c0-10c469c78149798d-5cc4db7f99be936f',
-//                'Content-Type' => 'application/json',
-//            ])->post('https://chatapi.viber.com/pa/send_message', [
-//                'receiver' => $userId,
-//                'type' => 'text',
-//                'text' => "Привет, {$userName}! 👋\n\nТы написал: {$text}\n\nТвой user.id:\n{$userId}",
-//            ]);
-//        }
-//    }
-//
+//Route::get('/viber/webhook', function (Request $request) {
+//    file_put_contents(storage_path('logs/test_viber.txt'), "Webhook вызван\n", FILE_APPEND);
 //    return response()->json(['status' => 0]);
 //});
+
+Route::post('/viber/webhook', function (Request $request) {
+    // Логируем всё в файл
+    Storage::disk('logs')->append('viber_log.txt', json_encode($request->all(), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+    // Проверяем, есть ли сообщение от пользователя
+    if (isset($request['event']) && $request['event'] === 'message') {
+        $userId = $request['sender']['id'] ?? null;
+        $userName = $request['sender']['name'] ?? 'Без имени';
+        $text = $request['message']['text'] ?? '';
+
+        if ($userId) {
+            Http::withHeaders([
+                'X-Viber-Auth-Token' => '479d6bb020e7d3c0-10c469c78149798d-5cc4db7f99be936f',
+                'Content-Type' => 'application/json',
+            ])->post('https://chatapi.viber.com/pa/send_message', [
+                'receiver' => $userId,
+                'type' => 'text',
+                'text' => "Привет, {$userName}! 👋\n\nТы написал: {$text}\n\nТвой user.id:\n{$userId}",
+            ]);
+        }
+    }
+
+    return response()->json(['status' => 0]);
+});
 /*
     curl -X POST \
   -H "X-Viber-Auth-Token: 479d6bb020e7d3c0-10c469c78149798d-5cc4db7f99be936f" \
