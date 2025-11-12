ed5649e6aab5668703e8555354b2466871d2f39b
igor <igorkri26@gmail.com>
2025-11-12 09:50:11 +0200

    feat: Додати виключення CSRF для вебхука Viber

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             'api/webhooks/asana',
+            '/viber/webhook',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
